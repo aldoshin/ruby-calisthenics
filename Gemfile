7505b07309cfac3bc76a7c23d950a3510e6f45cc
Aldo Perez <aldoshin@gmail.com>
2014-11-11 22:40:21 +0000

    fun with string completed

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'autotest', '~> 4.4.6'
+gem 'rspec', '~> 3.1.0'
--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,25 @@
 module FunWithStrings
   def palindrome?
+    downClean = self.downcase.gsub(/[\W\d]/, '')
+    downClean == downClean.reverse
     # your code here
   end
+  
   def count_words
-    # your code here
+    self.downcase.gsub(/[^\w\s]/, '').split.inject(Hash.new(0)) do |acc, el|
+      key = el.downcase
+      acc[key] = acc[key] + 1
+      acc
+    end
   end
+  
   def anagram_groups
-    # your code here
+    hash = self.split.inject(Hash.new([])) do |acc, word|
+      key = word.chars.sort.join
+      acc[key] = acc[key] + [word]
+      acc
+    end
+    hash.values
   end
 end
 
@@ -15,3 +28,10 @@ end
 class String
   include FunWithStrings
 end
+
+# puts "Hello"
+# empty_anam = "".anagram_groups
+# puts "#{empty_anam}"
+# x_anam ="scream cars for four scar creams".anagram_groups
+# puts "#{x_anam}"
+
